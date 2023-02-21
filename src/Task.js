24481c2f67e542397eeaed6654a804f14bb806ed
Bertan Sogutlu <bertansogutlu@gmail.com>
2023-02-22 00:55:40 +0300

    task component span class eklendi

--- src/Task.js
+++ src/Task.js
@@ -1,19 +1,24 @@
 import React from "react";
-import { formatDistanceToNow } from 'date-fns';
+import { formatDistanceToNow, differenceInDays } from 'date-fns';
 import tr from 'date-fns/locale/tr'
 
 
 const Task = ({ taskObj, onComplete }) => {
 
+  const DistanceToNow = formatDistanceToNow(
+    new Date(taskObj.deadline),
+    { addSuffix: true, locale: tr },
+  )
+
+  const distanceInDays = differenceInDays(
+    new Date(taskObj.deadline),
+    new Date()
+  )
+
   return (
     <div className="task">
       <h3>{taskObj.title}</h3>
-      <div className="deadline">son teslim: <span>{
-        formatDistanceToNow(
-          new Date(taskObj.deadline),
-          { addSuffix: true, locale: tr },
-        )
-        }</span></div>
+      <div className="deadline">son teslim: <span className={(distanceInDays < 3) ? "urgent" : "normal"}>{DistanceToNow}</span></div>
       <p>{taskObj.description}</p>
       <div>
         {taskObj.people.map((p) => (
